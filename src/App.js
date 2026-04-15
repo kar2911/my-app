ef7c6be56bde81cb8b8660af5d59932ae809ce62
unknown <karthikjangam75@gmail.com>
2026-04-15 18:13:03 +0530

    changed the ui

--- src/App.js
+++ src/App.js
@@ -3,44 +3,20 @@ import "./App.css";
 
 function App() {
   return (
-    <div style={styles.container}>
+    <div className="container">
       <h1>🚀 CI/CD SUCCESS</h1>
       <h2>Deployed using GitHub Actions + EC2</h2>
 
-      <div style={styles.box}>
+      <div className="box">
         <p>🔥 This change came from GitHub push</p>
         <p>⏱ Time: {new Date().toLocaleString()}</p>
       </div>
 
-      <button style={styles.button} onClick={() => alert("Pipeline Working ✅")}>
+      <button onClick={() => alert("Pipeline Working ✅")}>
         Click Me
       </button>
     </div>
   );
 }
-
-const styles = {
-  container: {
-    textAlign: "center",
-    marginTop: "50px",
-    fontFamily: "Arial",
-  },
-  box: {
-    margin: "20px auto",
-    padding: "20px",
-    width: "320px",
-    border: "2px solid black",
-    borderRadius: "10px",
-  },
-  button: {
-    padding: "10px 20px",
-    fontSize: "16px",
-    backgroundColor: "green",
-    color: "white",
-    border: "none",
-    borderRadius: "5px",
-    cursor: "pointer",
-  },
-};
 
 export default App;
